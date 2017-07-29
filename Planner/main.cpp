64123d13a41092e67aa8c1557b83af94be12f60d
Chris O'Neill <chris.s.oneill@o2.co.uk>
2017-07-29 21:35:52 +0100

    Initial Commit

---
+++ Planner/main.cpp
@@ -0,0 +1,16 @@
+// Daily Planner 2017 
+// Author : Chris O'Neill
+// File : src/main.cpp
+
+#include <iostream>
+
+using namespace std;
+
+bool g_bDebugMode = true;
+
+int main()
+{
+	//Open program
+	if (g_bDebugMode)cout << "Program Started" << endl;
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Daily-Planner
+A simple program for families to organise daily things
